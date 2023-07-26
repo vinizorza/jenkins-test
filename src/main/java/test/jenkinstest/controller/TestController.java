f9e9cf021a7c6a6e9c419e7c5d64a5be4c4fe856
Vinicius Zorzanelli <vinizorza@hotmail.com>
2023-07-26 14:36:56 +0100

    Initial commit

---
+++ src/main/java/test/jenkinstest/controller/TestController.java
@@ -0,0 +1,16 @@
+package test.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "hello world";
+    }
+
+}
